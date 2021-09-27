8f8dd26933d9ddb33dd84dd7f8420b721d875661
pakad <pakad@mail.ru>
2021-09-27 20:29:47 +0300

    lesson6: Свой ActionBar + второе Activity

--- app/src/main/java/ru/dudar/notebook/NotesListActivity.java
+++ app/src/main/java/ru/dudar/notebook/NotesListActivity.java
@@ -2,18 +2,25 @@ package ru.dudar.notebook;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class NotesListActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notes_list);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
@@ -25,7 +32,9 @@ public class NotesListActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.add_note) {
-            Toast.makeText(this, "Нажата клавиша", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, NoteEditActivity.class);
+            startActivity(intent);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
