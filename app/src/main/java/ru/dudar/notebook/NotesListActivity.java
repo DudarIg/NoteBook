97dea61bd6fedffc1388b828067f78904b7b2b9f
pakad <pakad@mail.ru>
2021-09-27 19:54:27 +0300

    lesson6: Создал меню добавления заметки

--- app/src/main/java/ru/dudar/notebook/NotesListActivity.java
+++ app/src/main/java/ru/dudar/notebook/NotesListActivity.java
@@ -1,8 +1,12 @@
 package ru.dudar.notebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NotesListActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class NotesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_note) {
+            Toast.makeText(this, "Нажата клавиша", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
